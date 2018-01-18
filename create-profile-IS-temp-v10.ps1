0f05f8a1c210893817dfe23e6e2c36b5bdbeaea4
Anton Fedorov <anton.fedorov@hpe.com>
2018-01-18 15:55:12 +0300

    Update create-profile-IS-temp-v10.ps1

--- create-profile-IS-temp-v10.ps1
+++ create-profile-IS-temp-v10.ps1
@@ -1,4 +1,4 @@
-# This is first version (1.0) of Profile creation from Template script
+# This is second version (2.0) of Profile creation from Template script
 #
 #--------------------uncomment this if you want to connect to OV------------
 #write-host -ForegroundColor Yellow "You need to autorize at OneView.."
@@ -9,14 +9,45 @@
 #Connect-HPOVMgmt -Hostname $OVIP1 -UserName $OVuser1 -Password $OVpass11
 #--------------------uncomment this if you want to connect to OV------------
 
-$Name1 = Read-Host 'Whhat is the name of ProfileTemplate?'
-$ProfileName1 = Read-Host 'What is profile prefered Name'
-$Numofpr1 = Read-Host 'How much Server Profiles do you want to create?'
+$Name0=Get-HPOVServerProfileTemplate
+$Name10=$Name0.Name
+$Name100=$Name0.Count
 
+if ($Name100 -gt 1)
+{
+# Show Server Profile Templates on the screen
+for ($j=0;$j -lt $Name100;$j++)
+{
+Write-Host -ForegroundColor DarkGreen "$j)" $Name10[$j]
+}
+
+#Let Customer choice what Template he want to use for Deployment
+$Name100=$Name0.Count-1
+$Num1=Read-Host "What Number of Profile Template you want to use 0-$Name100 ?"
+$Name1=$Name0[$Num1].Name
+$Name1
+Write-Host -ForegroundColor Cyan "==========================="
+Write-Host -ForegroundColor Cyan "We'll use - $Name1 - Template"
+Write-Host -ForegroundColor Cyan "==========================="
+}
+
+else
+{
+$Name1=$Name0[0].Name
+$Name1
+Write-Host -ForegroundColor Cyan "==========================="
+Write-Host -ForegroundColor Cyan "You have only 1 Profile Template. We'll use - $Name1 - Template"
+Write-Host -ForegroundColor Cyan "==========================="
+}
+
+#Let Customer choice - How much hosts he want to deploy
+$Numofpr1 = Read-Host 'How much Server Profiles do you want to create?'
 $spt=Get-HPOVServerProfileTemplate -Name $Name1
-for ($i=1;$i -like $Numofpr1;$i++)
+
+#Servers Deployment
+for ($i=1;$i -le $Numofpr1;$i++)
 {
 $server1=Get-HPOVServer -NoProfile -InputObject $spt | Select -first 1
-New-HPOVServerProfile -Name "$ProfileName1-0$i" -ServerProfileTemplate $spt -AssignmentType Server -Server $server1
-Get-HPOVServerProfile -Name "$ProfileName1-0$i" | Start-HPOVServer
+New-HPOVServerProfile -Name esxi-0$i -ServerProfileTemplate $spt -AssignmentType Server -Server $server1
+Get-HPOVServerProfile -Name esxi-0$i | Start-HPOVServer
 }
