6c78430730c421186ef43c806a4404cfe65b1466
Anton Fedorov <anton.fedorov@hpe.com>
2018-05-15 22:25:22 +0300

    Create 1-rest-showconfig.ps1

---
+++ restAPI/1-rest-showconfig.ps1
@@ -0,0 +1,25 @@
+#This is example of Get configuration RestAPI request with PowerShell
+#
+$json1 = @{
+      password='HP1nvent'
+      userName='administrator'
+   }
+
+
+$url = "https://16.52.176.102/rest/login-sessions"
+$header1 = @{}
+$header1.add("Accept-Language", "en_US")
+$header1.add("X-Api-Version", "600")
+$body1 = (ConvertTo-Json $json1)
+$result = Invoke-RestMethod -Uri $url -Body $body1 -Method Post -Headers $header1 -ContentType 'application/json'
+$session1=$result.sessionID
+$session1
+
+
+$url2 = "https://16.52.176.102/rest/global-settings"
+$header2 = @{}
+$header2.add("Accept-Language", "en_US")
+$header2.add("Auth", "$session1")
+$header2.add("X-Api-Version", "600")
+$result2 = Invoke-RestMethod -Uri $url2 -Method Get -Headers $header2
+return $result2
