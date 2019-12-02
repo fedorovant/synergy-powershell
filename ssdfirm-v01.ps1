d40b839dfa5a05b2baba81e31b9291d7b115875b
Anton Fedorov <anton.fedorov@hpe.com>
2019-12-02 15:49:31 +0300

    Update ssdfirm-v01.ps1
    Add affected SSD drives, only this one will be load at CSV.

--- ssdfirm-v01.ps1
+++ ssdfirm-v01.ps1
@@ -1,15 +1,19 @@
-# This is first version of Get SSD firmware data
+# This is first version of Get SSD firmware data. It got all your servers ILO and return CSV with affected SSD drives.
 # You need Redfish PS module and ILO4/ILO5 for this script
 #autorization
 $iloaddr="0"
-$user1="your login"
-$pass1="your pass"
-$csvpath='c:\temp\ssdfirm.csv' # path to the ilocsvfile, example you can find in the same folder
-$csvpathresult='c:\temp\ssdfirmresult1.csv' # resultfile, can be converted to excel
+$user1="hpadmin"
+$pass1="HP1nvent"
+$csvpath='c:\temp\ssdfirm.csv'
+$csvpathresult='c:\temp\ssdfirmresult1.csv'
 
 # Data array for results
 $ssdfirm= @()
-$ssdfirm+='Serials;Drives;Capacity;Firmware'
+$ssdfirm+='Server_Serial;Drive;Capacity;Drive_Model;Firmware;Status'
+
+#data of BAD models
+$baddata1= @("VO0480JFDGT","VO0960JFDGU","VO1920JFDGV","VO3840JFDHA","MO0400JFFCF","MO0800JFFCH","MO1600JFFCK","MO3200JFFCL","VO000480JWDAR","VO000960JWDAT","VO001920JWDAU","VO003840JWDAV","VO007680JWCNK","VO015300JWCNL","VK000960JWSSQ","VK001920JWSSR","VK003840JWSST","VK003840JWSST","VK007680JWSSU","VO015300JWSSV")
+$numofbad=0
 
 #Import ILO IP addresses
 $iloip=Import-Csv -Path $csvpath
@@ -29,12 +33,21 @@ foreach($iloaddr in $iloip.ip)
         foreach($drv in $sysdata.Members.'@odata.id')
         {
         $sysData1 = Get-HPERedfishDataRaw -odataid $drv -Session $session -DisableCertificateAuthentication
-        if ($sysData1.MediaType -like "SSD"){
-            $ssdfirm+=$sysname.SerialNumber + ";" + $sysData1.MediaType + ";" + $sysData1.CapacityGB + ";" + $sysData1.FirmwareVersion.Current.VersionString
+        #if ($sysData1.MediaType -like "SSD")
+            foreach($model1 in $baddata1)
+            {
+                if ($sysData1.Model -like $model1)
+                {
+                $numofbad=$numofbad+1
+                $ssdfirm+=$sysname.SerialNumber + ";" + $sysData1.MediaType + ";" + $sysData1.CapacityGB + ";" + $sysData1.Model +";" + $sysData1.FirmwareVersion.Current.VersionString + ";BAD"
+                }
             }
         } 
     }
 }
-
-return $ssdfirm | Out-File $csvpathresult 
+if ($numofbad -eq 0){Write-Host -ForegroundColor Green "congratulations! You have no any affected SSD drives"}
+    else 
+    {
+    $ssdfirm | Out-File $csvpathresult
+    Write-Host -ForegroundColor Red "unfortunately you have $numofbad affected SSD drives. The the list was loaded at $csvpathresult"}
 Disconnect-HPERedfish -DisableCertificateAuthentication -Session $Session
